6dd9d3e20bf3369477b9a48a3d3e85b26514d799
mushroomhe <992556471@qq.com>
2018-04-05 10:18:00 +0800

    成本效益评价

---
+++ evaluation/成本效益评价技术第二版/成本效益评价技术第二版/Form1.Designer.cs
@@ -0,0 +1,386 @@
+﻿namespace 成本效益评价技术第二版
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.UDYunXing = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbTouRu = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.UDBeginYear = new System.Windows.Forms.NumericUpDown();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.tbTieXianLv = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.jingLiRun = new System.Windows.Forms.Label();
+            this.huiShouQi = new System.Windows.Forms.Label();
+            this.huiBaoLv = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.jingXianZhi = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.UDYunXing)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.UDBeginYear)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(166, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "项目效益成本评价";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "项目运行年数：";
+            // 
+            // UDYunXing
+            // 
+            this.UDYunXing.Location = new System.Drawing.Point(99, 36);
+            this.UDYunXing.Name = "UDYunXing";
+            this.UDYunXing.Size = new System.Drawing.Size(82, 21);
+            this.UDYunXing.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(187, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "年";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(221, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 12);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "项目初期投入：";
+            // 
+            // tbTouRu
+            // 
+            this.tbTouRu.Location = new System.Drawing.Point(307, 35);
+            this.tbTouRu.Name = "tbTouRu";
+            this.tbTouRu.Size = new System.Drawing.Size(82, 21);
+            this.tbTouRu.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(395, 38);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 12);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "元";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 70);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(89, 12);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "项目起始年份：";
+            // 
+            // UDBeginYear
+            // 
+            this.UDBeginYear.Location = new System.Drawing.Point(99, 68);
+            this.UDBeginYear.Maximum = new decimal(new int[] {
+            2018,
+            0,
+            0,
+            0});
+            this.UDBeginYear.Minimum = new decimal(new int[] {
+            1900,
+            0,
+            0,
+            0});
+            this.UDBeginYear.Name = "UDBeginYear";
+            this.UDBeginYear.Size = new System.Drawing.Size(82, 21);
+            this.UDBeginYear.TabIndex = 9;
+            this.UDBeginYear.Value = new decimal(new int[] {
+            2000,
+            0,
+            0,
+            0});
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(187, 70);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(17, 12);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "年";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(223, 70);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(131, 12);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "贴现率（小/百分数）：";
+            // 
+            // tbTieXianLv
+            // 
+            this.tbTieXianLv.Location = new System.Drawing.Point(351, 67);
+            this.tbTieXianLv.Name = "tbTieXianLv";
+            this.tbTieXianLv.Size = new System.Drawing.Size(82, 21);
+            this.tbTieXianLv.TabIndex = 12;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(39, 95);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(243, 150);
+            this.dataGridView1.TabIndex = 13;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "年份";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "收入";
+            this.Column2.Name = "Column2";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label9.Location = new System.Drawing.Point(12, 248);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(122, 12);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "成本效益评价如下：";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(37, 273);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(101, 12);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "项目的净利润为：";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(187, 273);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(17, 12);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "元";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(37, 305);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(101, 12);
+            this.label12.TabIndex = 18;
+            this.label12.Text = "项目的回收期是第";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(176, 305);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(17, 12);
+            this.label13.TabIndex = 20;
+            this.label13.Text = "年";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(37, 336);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(113, 12);
+            this.label14.TabIndex = 21;
+            this.label14.Text = "项目投资回报率为：";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(37, 366);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(77, 12);
+            this.label15.TabIndex = 23;
+            this.label15.Text = "项目净现值：";
+            // 
+            // jingLiRun
+            // 
+            this.jingLiRun.AutoSize = true;
+            this.jingLiRun.Location = new System.Drawing.Point(144, 273);
+            this.jingLiRun.Name = "jingLiRun";
+            this.jingLiRun.Size = new System.Drawing.Size(0, 12);
+            this.jingLiRun.TabIndex = 25;
+            this.jingLiRun.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // huiShouQi
+            // 
+            this.huiShouQi.AutoSize = true;
+            this.huiShouQi.Location = new System.Drawing.Point(150, 305);
+            this.huiShouQi.Name = "huiShouQi";
+            this.huiShouQi.Size = new System.Drawing.Size(0, 12);
+            this.huiShouQi.TabIndex = 26;
+            this.huiShouQi.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // huiBaoLv
+            // 
+            this.huiBaoLv.AutoSize = true;
+            this.huiBaoLv.Location = new System.Drawing.Point(144, 336);
+            this.huiBaoLv.Name = "huiBaoLv";
+            this.huiBaoLv.Size = new System.Drawing.Size(0, 12);
+            this.huiBaoLv.TabIndex = 27;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button1.Font = new System.Drawing.Font("幼圆", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(307, 169);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 38);
+            this.button1.TabIndex = 28;
+            this.button1.Text = "进行项目评价";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // jingXianZhi
+            // 
+            this.jingXianZhi.AutoSize = true;
+            this.jingXianZhi.Location = new System.Drawing.Point(120, 366);
+            this.jingXianZhi.Name = "jingXianZhi";
+            this.jingXianZhi.Size = new System.Drawing.Size(0, 12);
+            this.jingXianZhi.TabIndex = 29;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(454, 549);
+            this.Controls.Add(this.jingXianZhi);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.huiBaoLv);
+            this.Controls.Add(this.huiShouQi);
+            this.Controls.Add(this.jingLiRun);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.tbTieXianLv);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.UDBeginYear);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbTouRu);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.UDYunXing);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "项目效益成本评价";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.UDYunXing)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.UDBeginYear)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown UDYunXing;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbTouRu;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown UDBeginYear;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox tbTieXianLv;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label jingLiRun;
+        private System.Windows.Forms.Label huiShouQi;
+        private System.Windows.Forms.Label huiBaoLv;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label jingXianZhi;
+    }
+}
+
---
+++ evaluation/成本效益评价技术第二版/成本效益评价技术第二版/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace 成本效益评价技术第二版
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //dataGridView1.RowsAdded += new DataGridViewRowsAddedEventHandler(dgvEvent);
+            //dataGridView1.RowsRemoved -= new DataGridViewRowsRemovedEventHandler(dgvEvent);
+        }
+
+        //private void dgvEvent(object sender, DataGridViewRowsAddedEventArgs e)
+        //{
+        //    for (int x = 0; x < dataGridView1.Rows.Count; x++)
+        //    {
+        //        dataGridView1.AllowUserToAddRows = true;
+        //        if (x > 7)
+        //        {
+        //            dataGridView1.AllowUserToAddRows = false;
+        //            MessageBox.Show("收入情况已输入完毕！");
+        //        }
+        //        //else
+        //        //{
+        //        //    dataGridView1.AllowUserToAddRows = true;
+        //        //}
+        //    }
+        //    //dataGridView1.AllowUserToAddRows = false;
+        //    //if (dataGridView1.RowCount < 5)
+        //    //{
+        //    //    dataGridView1.AllowUserToAddRows = true;
+        //    //    //MessageBox.Show("收入情况已输入完毕！");
+        //    //}                                                
+        //    //else
+        //    //{
+        //    //    //MessageBox.Show("收入情况已输入完毕！");
+        //    //    dataGridView1.AllowUserToAddRows = true;
+                
+        //    //}
+        //}
+
+        //计算之前首先判断是否为空
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int year,beginYear;
+            double touRu;
+            String tieXianLv="";
+
+
+            //项目运行年数，项目初期投入（需判断是否是数值形式），以及项目起始年份，贴现率不能为空
+            if (int.TryParse(UDYunXing.Text, out year) && int.TryParse(UDBeginYear.Text, out beginYear) && double.TryParse(tbTouRu.Text, out touRu))
+            {
+                if (tbTieXianLv.Text == "")
+                {
+                    MessageBox.Show("请输入贴现率！");
+                }
+                else
+                {
+                    for (int a = 1; a <= year; a++)
+                    {
+                        if (dataGridView1.RowCount==year)
+                        {
+                            //开始计算各项属性值
+                            tieXianLv = tbTieXianLv.Text;
+                            calculate();
+                        }
+                        else
+                        {
+                            MessageBox.Show("请将所有年份收入情况输入完毕！");
+                            break;
+                        }
+                    }
+                       
+                }
+            }
+            else
+            {
+                MessageBox.Show("请正确输入各项属性值！");
+            }
+
+
+        }
+
+
+        //计算各项属性函数
+        public void calculate()
+        {
+            //项目运行年数
+            int year;
+            year =int.Parse( UDYunXing.Text);
+
+            //定义每年收入的数组
+            ArrayList shouRu = new ArrayList();
+            //初期投入
+            double touRu;
+            touRu = double.Parse(tbTouRu.Text);
+            shouRu.Add(-touRu);
+           
+            //将每年收入情况输入
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                shouRu.Add(dataGridView1.Rows[i].Cells["Column2"].Value);
+            }
+
+            //贴现率
+            String s;
+            double lv=0;
+            s = tbTieXianLv.Text;
+            int pos = s.IndexOf("%");
+            int length = s.Length;
+            if (pos == -1)//如果没有百分号
+            {
+                lv = double.Parse(s);//将字符串转换为double类型
+            }
+            else if (pos < length)//如果存在百分号
+            {
+                lv = (double.Parse(s.Substring(0,pos)))/100;
+            }
+
+            //成本效益评价:
+
+            //净利润
+            jingLiRun.Text = ""+NP(year,shouRu);
+            //回收期
+            huiShouQi.Text = "" + PP(year,shouRu);
+            //投资回报率
+            huiBaoLv.Text = ROI(year, shouRu);
+            //净现值
+            jingXianZhi.Text = NPV(year, shouRu,lv);
+        }
+
+
+        //净利润
+        public double NP(int year, ArrayList p)
+        {
+            double np = 0;
+            for (int a = 0; a <=year; a++)
+            {
+                np += (double.Parse(p[a].ToString()));
+            }
+            return np;
+        }
+
+        //回收期
+        public int PP(int year, ArrayList p)
+        {
+            double pp = 0;
+            int a;
+            for (a = 0; a <= year; a++)
+            {
+                pp += (double.Parse(p[a].ToString()));
+                if (pp >= 0)
+                    break;
+            }
+            return a;
+        }
+
+        //投资回报率
+        public String ROI(int year, ArrayList p)
+        {
+            //净利润
+            double np = NP(year, p);
+
+            double roi = 0;
+            roi = np / (-(double.Parse(p[0].ToString()))*year);
+            return roi.ToString("P");
+        }
+
+        //净现值
+        public String NPV(int year, ArrayList p, double lv)
+        {
+            //贴现因子（存储在数组中）
+            ArrayList f = new ArrayList();
+            f.Add(1);
+            for (int a = 1; a <= year; a++)
+            {
+                f.Add(1/(Math.Pow((1+lv),a)));
+            }
+
+            double z = 0;
+            String s="";
+            for (int a = 0; a <= year; a++)
+            {
+                s += "第" + a + "年的净现值为：" + ((double.Parse(p[a].ToString())) * (double.Parse(f[a].ToString()))).ToString("f4") + "元\r\n\r\n";
+                z += (double.Parse(p[a].ToString())) * (double.Parse(f[a].ToString()));
+            }
+            s += "总净现值为：" + z.ToString("f4") + "元";
+            return s;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.UserAddedRow += new DataGridViewRowEventHandler(dataGridView1_UserAddedRow);
+        }
+        private int m_AddCount = 0;
+        private void dataGridView1_UserAddedRow(object sender, DataGridViewRowEventArgs e)
+        {
+            m_AddCount++;
+            this.dataGridView1.Rows[m_AddCount-1].Cells["Column1"].Value = UDBeginYear.Value + m_AddCount - 1;
+            if (m_AddCount == UDYunXing.Value) dataGridView1.AllowUserToAddRows = false;
+        }
+
+
+    }
+}
+
